ff149b21cd0e182a842d99aef70512d37607ae2e
howrs <a@how.rs>
2024-08-27 18:10:09 +0900

    init

---
+++ constants/urls.ts
@@ -0,0 +1,1 @@
+export const HOST = `rss.watch`
---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const config = {
+  reactStrictMode: true,
+  compiler: {
+    // removeConsole: true,
+  },
+}
+
+module.exports = config
